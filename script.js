f3ccce0fd0bf8d73a304f571db5434802f8ad54c
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2024-04-29 10:57:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const colorAzul = document.getElementById('ColorAzul1'); 
+const colorRojo = document.getElementById('colorRojo'); 
+const colorVerde = document.getElementById('colorVerde'); 
+const colorAmarillo = document.getElementById('colorAmarillo');
+
+let time = 0 
+const botonStart = document.getElementById('botonComenzar'); 
+
+
+colorAmarillo.addEventListener('click', function() {
+    console.log("Hola, pe");
+  
+    
+});
+
+colorAzul.addEventListener('click', function() {
+    console.log("Hola, mundo");
+});
+
+colorRojo.addEventListener('click', function() {
+    console.log("Hola, mundo");
+});
+
+
+colorVerde.addEventListener('click', function() {
+    console.log("Hola, mundo");
+});
+
